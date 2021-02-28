64dc91ba2c5fdeb7e0a1b3752617f649708ffe3f
Patrik Turi <patrik.turi.0xff@gmail.com>
2021-02-28 20:57:16 +0100

    Move ddcontrol operations to background threads

--- gui.py
+++ gui.py
@@ -2,11 +2,17 @@ from subprocess import run, PIPE, STDOUT
 import tkinter as tk
 from tkinter import messagebox
 import re
+from threading import Thread
+from concurrent.futures import ThreadPoolExecutor
 
 
 DEVICE_PATH = '/dev/i2c-1'
 BRIGHTNESS_ADDRESS = '0x10'
 CONTRAST_ADDRESS = '0x12'
+
+
+executor = ThreadPoolExecutor(max_workers=1)
+sliders = []
 
 
 class BrightnessSlider:
@@ -17,8 +23,16 @@ class BrightnessSlider:
         self.slider = tk.Scale(root, from_=0, to=100, orient=tk.HORIZONTAL, command=self.updateValue)
         self.slider.pack()
 
-        current_value = self.read_remote_value(address)
-        self.slider.set(current_value)
+        self.slider.set(0)
+
+        self.future = executor.submit(self.read_remote_value, address)
+
+    def checkInitFuture(self):
+        if self.future.done():
+            print(self.future.result())
+            self.slider.set(self.future.result())
+            return True
+        return False
 
     def read_remote_value(self, address):
         command = ['ddccontrol', '-r', address, f'dev:{DEVICE_PATH}']
@@ -61,10 +75,26 @@ class MainFrame(tk.Frame):
         self.label = tk.Label(self, text="Brightness")
         self.label.pack(padx=20, pady=20)
         self.slider = BrightnessSlider(self, BRIGHTNESS_ADDRESS)
+        sliders.append(self.slider)
 
         self.label = tk.Label(self, text="Contrast")
         self.label.pack(padx=20, pady=20)
         self.slider = BrightnessSlider(self, CONTRAST_ADDRESS)
+        sliders.append(self.slider)
+
+
+def tick():
+    global sliders
+    print('.')
+    to_remove = []
+    for slider in sliders:
+        if slider.checkInitFuture():
+            print('done')
+            to_remove.append(slider)
+
+    sliders = [slider for slider in sliders if slider not in to_remove]
+    if sliders:
+        root.after(100, tick)
 
 
 if __name__ == "__main__":
@@ -73,4 +103,8 @@ if __name__ == "__main__":
     main = MainFrame(root)
     main.pack(fill="both", expand=True)
 
+    root.after(100, tick)
     root.mainloop()
+
+# TODO: handler error messages
+# set value on thread as well
